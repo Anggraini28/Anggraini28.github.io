ff8dc48ac5c2769666cb1b6dc19b4acb8648c1ed
Anggraini28 <119483634+Anggraini28@users.noreply.github.com>
2022-11-30 20:25:58 +0700

    Add files via upload

---
+++ css/zk.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+
+sr.reveal(".home", { delay: 200, durasi: 200 });
+sr.reveal(".about", { delay: 200, durasi: 200 });
+sr.reveal(".products", { delay: 200, durasi: 200 });
+sr.reveal(".review", { delay: 200, durasi: 200 });
+sr.reveal(".formatOrder", { delay: 200, durasi: 200 });
+sr.reveal(".footer", { delay: 200, durasi: 200 });
+
